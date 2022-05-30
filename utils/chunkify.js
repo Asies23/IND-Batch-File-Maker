6f0aa4375844ed607124cd7562435ec70998545a
Asiesjhot Singh Chandowk <47882556+Asies23@users.noreply.github.com>
2022-05-30 22:45:34 +0530

    ♻️ Fix chunkify output

--- utils/chunkify.js
+++ utils/chunkify.js
@@ -6,24 +6,26 @@ let chunkedObj = [{}];
 let chunkedObjIndex = 0;
 
 let chunkify = (obj) => {
+  console.log(obj);
   for (char in obj) {
     console.log(char);
     let singleCharCount = 0;
     for (let i = obj[char]; i > 0; i--) {
       width += setWidth(char);
-      if (width < 1150) {
+      if (width < 1000) {
         if (singleCharCount == 0) {
           chunkedObj[chunkedObjIndex][char] = 0;
         }
-        chunkedObj[chunkedObjIndex][char]++;
+        chunkedObj[chunkedObjIndex][char] += 1;
       } else {
         width = setWidth(char);
         chunkedObj.push({});
         console.log(chunkedObj);
         chunkedObjIndex++;
         chunkedObj[chunkedObjIndex][char] = 0;
-        chunkedObj[chunkedObjIndex][char]++;
+        chunkedObj[chunkedObjIndex][char] += 1;
       }
+      singleCharCount++;
     }
   }
   return chunkedObj;
